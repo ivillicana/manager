52e32abd681c3dd3b7eee147dd8d51e5e9ff0763
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-07 22:41:07 -0400

    add firebase

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAS-d7d800zOKCpuBVZt_AouPjsnWsTBdk",
+      authDomain: "manager-7f1ea.firebaseapp.com",
+      databaseURL: "https://manager-7f1ea.firebaseio.com",
+      projectId: "manager-7f1ea",
+      storageBucket: "manager-7f1ea.appspot.com",
+      messagingSenderId: "950624533137",
+      appId: "1:950624533137:web:daee17bb0783acf1"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Manager
+          </Text>
+        </View>
+      </Provider>
+    );
+  };
+}
+
+export default App;
