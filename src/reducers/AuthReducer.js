352adc59d93b0051a404855f3a4f03b1b522f11e
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-08 13:31:02 -0400

    change loading false

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -21,9 +21,9 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload, error: ''};
+      return { ...state, user: action.payload, error: '', loading: false };
     case LOGIN_USER_FAIL:
-      return { ...state, error: 'Authentication Failed', password: '' };
+      return { ...state, error: 'Authentication Failed', password: '', loading: false };
     case LOGIN_USER_PENDING:
       return { ...state, loading: true, error: '' }
     default:
