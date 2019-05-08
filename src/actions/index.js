ce9992f8a1554ca3649bc2c8c63a60621244a02c
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-08 12:44:55 -0400

    add linting

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,6 +21,12 @@ export const loginUser = ({ email, password}) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
         dispatch({ type: LOGIN_USER_SUCCESS, payload: user});
+      })
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then (user => {
+          dispatch({ type:  LOGIN_USER_SUCCESS, payload: user });
+        })
       });
   };
 };
