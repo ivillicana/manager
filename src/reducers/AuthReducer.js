c058c38addd511804bed022a3ee82919fddf01bc
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-08 13:30:28 -0400

    add reducer case

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,14 +2,16 @@ import {
   EMAIL_CHANGED, 
   PASSWORD_CHANGED, 
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAIL 
+  LOGIN_USER_FAIL, 
+  LOGIN_USER_PENDING
 } from '../actions/types'
 
 const INITIAL_STATE = { 
   email: '', 
   password: '', 
   user: null,
-  error: ''
+  error: '',
+  loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -19,9 +21,11 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload, error: ''}
+      return { ...state, user: action.payload, error: ''};
     case LOGIN_USER_FAIL:
-      return { ...state, error: 'Authentication Failed', password: '' }
+      return { ...state, error: 'Authentication Failed', password: '' };
+    case LOGIN_USER_PENDING:
+      return { ...state, loading: true, error: '' }
     default:
       return state;
   }
