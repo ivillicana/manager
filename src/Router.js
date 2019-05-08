6813046bddc37307d07509e1e84a8fc466a6d489
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-08 17:11:45 -0400

    add employee create to router

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -18,7 +19,12 @@ const RouterComponent = () => {
           key="employeeList" 
           component={EmployeeList} 
           title="Employees" 
-        />
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate} 
+            title="Create Employee"
+          />
         </Scene>
       </Scene>
     </Router>
