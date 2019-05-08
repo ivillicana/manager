0c0ecc090f75ad5fcbc8ad54293513605e8ef9ae
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-08 17:51:26 -0400

    add Picker fuinction

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+  createPickers() {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    return days.map(day => <Picker.Item key={day} label={day} value={day} />)
+  }
+
   render() {
     return (
       <Card>
@@ -27,17 +32,13 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
+            style={{ flex: 1 }}
             selectedValue={this.props.shift}
             onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
           >
-            <Picker.Item label="Monday" value="Monday" />
-            <Picker.Item label="Tuesday" value="Tuesday" />
-            <Picker.Item label="Wednesday" value="Wednesday" />
-            <Picker.Item label="Thursday" value="Thursday" />
-            <Picker.Item label="Friday" value="Friday" />
-            <Picker.Item label="Saturday" value="Saturday" />
-            <Picker.Item label="Sunday" value="Sunday" />
+            {this.createPickers()}
           </Picker>
         </CardSection>
 
@@ -50,6 +51,13 @@ class EmployeeCreate extends Component {
     );
   }
 }
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+};
 
 const mapStateToProps = state => {
   const { name, phone, shift } = state.employeeForm
